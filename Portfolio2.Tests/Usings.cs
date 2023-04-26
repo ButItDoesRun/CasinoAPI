edf69b8e9066bc83c64ca5b27b604837ecb63e35
ButItDoesRun <90749391+ButItDoesRun@users.noreply.github.com>
2023-04-27 00:21:24 +0200

    Delete Portfolio2.Tests directory

--- Portfolio2.Tests/Usings.cs
+++
@@ -1,8 +0,0 @@
-global using Xunit;
-global using DataLayer;
-global using DataLayer.DatabaseModel;
-global using System.Net;
-global using System.Text;
-global using Newtonsoft.Json;
-global using Newtonsoft.Json.Linq;
-
