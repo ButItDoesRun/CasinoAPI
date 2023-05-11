f21082a0ab9df5e46f5fdb1905d04902d2b0e984
Anne-Thea <annethearendtorff@gmail.com>
2023-05-11 14:20:19 +0200

    Removed game record from context (and disabled database logging)

--- DataService/CasinoDBContext.cs
+++ DataService/CasinoDBContext.cs
@@ -27,7 +27,6 @@ namespace DataLayer
 
         /* CASINO MODEL */
         public DbSet<Game>? Games { get; set; }
-        public DbSet<GameRecord>? GamesRecords { get; set; }
         public DbSet<Player>? Players { get; set; }
         public DbSet<Bet>? Bets { get; set; }
         public DbSet<Salt>? Salts { get; set; }
@@ -36,7 +35,7 @@ namespace DataLayer
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             base.OnConfiguring(optionsBuilder);
-            optionsBuilder.LogTo(Console.WriteLine, LogLevel.Information);
+            //optionsBuilder.LogTo(Console.WriteLine, LogLevel.Information);
             optionsBuilder.UseNpgsql(ConnectionString);
         }
 
@@ -65,15 +64,6 @@ namespace DataLayer
                 .HasOne(x => x.MoneyPot)
                 .WithOne(x => x.Game)
                 .HasForeignKey<Game>(x => x.Pid);
-
-            //GAME RECORDS
-            modelBuilder.Entity<GameRecord>().HasNoKey();
-            modelBuilder.Entity<GameRecord>().Property(x => x.Gid).HasColumnName("gid");
-            modelBuilder.Entity<GameRecord>().Property(x => x.Name).HasColumnName("name");
-            modelBuilder.Entity<GameRecord>().Property(x => x.MinBet).HasColumnName("minbet");
-            modelBuilder.Entity<GameRecord>().Property(x => x.MaxBet).HasColumnName("maxbet");
-            modelBuilder.Entity<GameRecord>().Property(x => x.Pid).HasColumnName("pid");
-            modelBuilder.Entity<GameRecord>().Property(x => x.Amount).HasColumnName("amount");
 
             //MONEYPOTS
             modelBuilder.Entity<MoneyPot>().ToTable("moneypot");
