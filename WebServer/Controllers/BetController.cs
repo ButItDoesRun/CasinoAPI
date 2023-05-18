7841f8bd973a0e66f385b193f7b4c09bbee5a7cc
MrMrThing <rasmus031299@gmail.com>
2023-05-18 11:26:23 +0200

    Seeing if null

--- WebServer/Controllers/BetController.cs
+++ WebServer/Controllers/BetController.cs
@@ -36,6 +36,12 @@ namespace WebServer.Controllers
         public IActionResult CreateBet(BetCreateModel createModel)
         {
             var bet = _dataServiceBet.CreateBet(createModel.PlayerName, createModel.Gid, createModel.Amount);
+            
+            if(bet == null)
+            {
+                return BadRequest();    
+            }
+
             var createBetModel = ConstructBetModel(bet);
             return Ok(createBetModel);
         }   
